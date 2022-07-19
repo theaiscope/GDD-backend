e016a76e70031b44db21f4dc20cf83b7319d4b56
Marcos Jordao <marcos.jordao@thoughtworks.com>
2022-07-19 17:43:03 +0200

    Move onNewUserCreated back to the index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,34 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
-import * as UserCreated from './user/user-created'
 import * as FacilityCreated from './facility/facility-created'
+import UserRecord = admin.auth.UserRecord
 
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp()
+const db = admin.firestore()
 
-export const onNewUserCreated = UserCreated.onNewUserCreated
+export const onNewUserCreated = functions
+  .region('europe-west1')
+  .auth.user()
+  .onCreate((user) => {
+    try {
+      console.log('>>>>>>> New user created:', user.uid)
+      return saveMicroscopistToFirestore(user)
+    } catch (error) {
+      functions.logger.error('Error writing document: ', error)
+      return null
+    }
+  })
+
+const saveMicroscopistToFirestore = (user: UserRecord) =>
+  db
+    .collection('microscopists')
+    .doc(user.uid)
+    .set({
+      enabled: false,
+    })
+    .then(function () {
+      console.log('>>>> saveMicroscopistToFirestore called')
+      functions.logger.log('Document successfully written!')
+    })
 
 export const onNewFacilityCreated = FacilityCreated.onNewFacilityCreated
