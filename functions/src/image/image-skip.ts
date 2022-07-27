2b4495b4c980ea23ebcb9e56a5311bdcf4d7fcb4
Marcos Jordao <marcos.jordao@thoughtworks.com>
2022-07-27 15:44:49 +0200

    Format code
    Co-authored-by: Sendami Luque <sendami.luque@thoughtworks.com>

--- functions/src/image/image-skip.ts
+++ functions/src/image/image-skip.ts
@@ -1,28 +1,24 @@
-import * as functions from 'firebase-functions';
-import { Collections } from '../data/collections';
-import { db } from '../index';
+import * as functions from 'firebase-functions'
+import { Collections } from '../data/collections'
+import { db } from '../index'
 
-export const skipImage = functions
-    .region('europe-west1')
-    .https
-    .onCall(async (data, context) => {
-        if (!context.auth) {
-            throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.');
-        }
+export const skipImage = functions.region('europe-west1').https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.')
+  }
 
-        const image = await db.collection(Collections.IMAGES).doc(data.imageId).get();
-        const labellerId = context.auth.uid;
-        const labellers = [...image.get('labellers'), labellerId];
+  const image = await db.collection(Collections.IMAGES).doc(data.imageId).get()
+  const labellerId = context.auth.uid
+  const labellers = [...image.get('labellers'), labellerId]
 
-        return db
-            .collection(Collections.IMAGES)
-            .doc(data.imageId)
-            .set({ labellers }, { merge: true })
-            .then(() => {
-                functions.logger.log('Image successfully updated!');
-            })
-            .catch((error) => {
-                functions.logger.error('Error updating image: ', error);
-            });
-
-    });
+  return db
+    .collection(Collections.IMAGES)
+    .doc(data.imageId)
+    .set({ labellers }, { merge: true })
+    .then(() => {
+      functions.logger.log('Image successfully updated!')
+    })
+    .catch((error) => {
+      functions.logger.error('Error updating image: ', error)
+    })
+})
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,14 @@
-import * as admin from 'firebase-admin';
-import * as FacilityCreated from './facility/facility-created';
-import * as UserCreated from './user/user-created';
-import { skipImage as skipImageFunction } from './image/image-skip';
+import * as admin from 'firebase-admin'
+import * as FacilityCreated from './facility/facility-created'
+import * as UserCreated from './user/user-created'
+import { skipImage as skipImageFunction } from './image/image-skip'
 
-admin.initializeApp();
+admin.initializeApp()
 
-export const db = admin.firestore();
+export const db = admin.firestore()
 
-export const onNewUserCreated = UserCreated.onNewUserCreated;
+export const onNewUserCreated = UserCreated.onNewUserCreated
 
-export const onNewFacilityCreated = FacilityCreated.onNewFacilityCreated;
+export const onNewFacilityCreated = FacilityCreated.onNewFacilityCreated
 
-export const skipImage = skipImageFunction;
+export const skipImage = skipImageFunction
